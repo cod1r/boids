6c3acbdbaa793e48af01320248455ffdcd4762f8
cod1r <jasonho353@gmail.com>
2024-07-01 02:13:44 -0500

    WIP webgl setup

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,64 @@
 const canvas = document.getElementById("canvas")! as HTMLCanvasElement
 
-const ctx = canvas.getContext("webgl")
+const gl = canvas.getContext("webgl")
+
+if (!gl) {
+  throw Error("gl context is null")
+}
+
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+const vertexShaderSrc = `
+attribute vec3 position;
+void main() {
+  gl_Position = vec4(position, 1.0);
+}
+`
+const fragmentShaderSrc = `
+void main() {
+  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+}
+`
+
+const vertexShader = gl.createShader(gl.VERTEX_SHADER)!
+const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER)!
+gl.shaderSource(vertexShader, vertexShaderSrc)
+gl.shaderSource(fragmentShader, fragmentShaderSrc)
+gl.compileShader(vertexShader)
+gl.compileShader(fragmentShader)
+
+const program = gl.createProgram()!
+gl.attachShader(program, vertexShader)
+gl.attachShader(program, fragmentShader)
+gl.linkProgram(program)
+gl.useProgram(program)
+
+const buffer = gl.createBuffer()!
+gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
+
+const positionLocation = gl.getAttribLocation(program, "position")
+
+if (positionLocation === -1) {
+  throw Error("position attribute -1")
+}
+
+gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 2 * 4, 0)
+gl.enableVertexAttribArray(positionLocation)
+
+let vertices = new Float32Array(2 * 3)
+vertices[0] = 0.0
+vertices[1] = 0.5
+
+vertices[2] = -0.5
+vertices[3] = -0.5
+
+vertices[4] = 0.5
+vertices[5] = -0.5
+
+gl.bufferData(gl.ARRAY_BUFFER, vertices.buffer, gl.DYNAMIC_DRAW)
+
+//const loop = () => {
+//  requestAnimationFrame(loop)
+//}
+//requestAnimationFrame(loop)
